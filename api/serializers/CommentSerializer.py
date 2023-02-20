502d1d415cdcf7d8832def00f8970983fcbf0842
alifirhas <irhasalif@gmail.com>
2023-02-20 08:38:27 +0700

    :fire: Delete Comment module

--- api/serializers/CommentSerializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from api.models import Comment
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ['id', 'created_at', 'updated_at']
